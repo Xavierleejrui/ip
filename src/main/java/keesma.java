76417dcb67e0dc5b618ca919e089204b3b20d977
Xavierleejrui <e1121800@u.nus.edu>
2025-01-22 22:13:35 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/keesma.java
@@ -0,0 +1,15 @@
+public class keesma {
+    public static void main(String[] args) {
+        String line = ("____________________________________________________________");
+        String name = "Keesma";
+        String question = "What can I do for you babes?";
+        String greetings = "Helloooooo I'm " + name + "\n" + question;
+        String goodbye = "Bye, Hope to see you again soon, you smell great by the way, although you did smell better last night hehe ^ W ^ ";
+
+        System.out.println(line);
+        System.out.println(greetings);
+        System.out.println(line);
+        System.out.println(goodbye);
+        System.out.println(line);
+    }
+}
